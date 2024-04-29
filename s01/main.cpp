285b4866015998a715306e6a990f9fd40a5044af
K0RADO <161463686+K0RADO@users.noreply.github.com>
2024-04-29 12:56:58 +0800

    Add files via upload

---
+++ s01/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int i=12, sum=i+3;
+float g=2.1, dif=g-0.3;
+short o=6 ,mult=o*2;
+double r=2.8, div=r/4;
+
+cout <<sum<<endl;
+cout <<dif<<endl;
+cout <<mult<<endl;
+cout <<div<<endl;
+return 0 ;
+}
